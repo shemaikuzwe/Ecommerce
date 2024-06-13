363e887f32194189cfc7017114fef48423e89858
Shema Elisa <163114861+Ikuzweshema@users.noreply.github.com>
2024-06-13 09:34:07 +0200

    fixing errors

--- next.config.mjs
+++ next.config.mjs
@@ -8,7 +8,10 @@ const nextConfig = {
   },
   typescript:{
     ignoreBuildErrors:true,
-  }
+  },
+  experimental: {
+    missingSuspenseWithCSRBailout: false,
+  },
 };
 
 export default nextConfig;
