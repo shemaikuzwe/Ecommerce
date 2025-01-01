32a1a19d9fb2926bb2dcafd96f688d4838a3e8f6
shemaIkuzwe <eshemaikuzwe@gmail.com>
2025-01-01 12:00:13 +0200

    fixing build erros

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import {  Poppins } from "next/font/google";
 import "./globals.css";
-import Providers from "@/store/providers";
 import { SessionProvider } from "next-auth/react";
 import { ThemeProvider } from "@/components/providers/theme-provider";
 const poppins = Poppins({ subsets: ["latin"], weight: ["400"] });
